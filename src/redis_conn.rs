de5cba8fad9d35b72bb90534a10a5f52caf93c8e
Andrey Filipov <andrey@filipov.dev>
2020-02-20 01:33:04 +0300

    Init

--- src/redis_conn.rs
+++ src/redis_conn.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 pub fn redis_connection() -> redis::Connection {
-    let host  = env::var("REDIS_URL").is_err();
+    let host  = env::var("REDIS_URL").unwrap();
 
     match redis::Client::open(host) {
         Err(e) => panic!("{:#?}", e),
